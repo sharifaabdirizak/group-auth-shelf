10e421f1bdc0f260a55bddd9b05711a9d69d2862
JDNafz <nafziger10@gmail.com>
2023-10-05 12:08:27 -0500

    starting shelf saga

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+
 
 function ShelfPage() {
+  const [url, setUrl] = useState();
+  const [description, setDescription] = useState();
+  const dispatch = useDispatch();
+
+
+  const submitForm = () => {
+    dispatch({type: "ADD_ITEM_TO_SHELF", payload: { url: url, description: description}})
+  }
   return (
     <div className="container">
       <h2>Shelf</h2>
       <p>All of the available items can be seen here.</p>
+      <form onSubmit={submitForm}>
+        <label>url</label>
+        <input onChange={(e) => setUrl(e.target.value)} value={url} />
+
+        <label>description</label>
+        <input
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+        />
+      </form>
     </div>
   );
 }
